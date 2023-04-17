d68cb887c97fe61d38e29d6142e3da93989e47cb
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-17 16:20:51 -0300

    <ADD> class GetPhotoDefault

---
+++ src/main/java/com/sirun/user/entity/GetPhotoDefault.java
@@ -0,0 +1,10 @@
+
+
+package com.sirun.user.entity;
+
+public class GetPhotoDefault {
+
+ static public String get(){
+  return "https://thumbs.dreamstime.com/b/marcador-de-foto-avatar-perfil-predeterminado-ilustraci%C3%B3n-vectorial-189495158.jpg";
+  } 
+}
