6d0b0254433d4beef34f053a38a566b434a4bbeb
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-17 17:13:14 -0300

    <REFACTOR> class User by Interface User and methods

--- src/main/java/com/sirun/user/model/User.java
+++ src/main/java/com/sirun/user/model/User.java
@@ -1,33 +1,22 @@
-package com.sirun.user.entity;
-
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@ToString
-class User {
-  @Getter @Setter
-  private String name;
-  @Getter @Setter
-  private String lastName;
-  @Getter @Setter
-  private String email;
-  @Getter @Setter
-  private String password;
-  @Getter @Setter
-  private Integer age;
-  @Getter @Setter
-  private String photo = GetPhotoDefault.get();
-  @Getter
-  private Roles  rol = Roles.CLIENT;
-
-  public User(String name, String lastName, String email, String password, Integer age) {
-    this.name = name;
-    this.lastName = lastName;
-    this.email = email;
-    this.password = password;
-    this.age = age;
-  }
+package com.sirun.user.model;
 
 
+public interface User {
+
+  String getName();
+  String getLastName();
+  String getEmail();
+  String getPassword();
+  String getPhoto();
+  Integer getAge();
+  Roles getRol();
+
+
+  void setName(String name);
+  void setLastName(String lastName);
+  void setEmail(String email);
+  void setPassword(String password);
+  void setPhoto(String photo);
+  void setAge(Integer age);
+  
 }
