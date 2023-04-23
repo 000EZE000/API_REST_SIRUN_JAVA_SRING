c9497d3e98be78efbb83b1112ffcf5f017f333dc
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-23 00:32:53 -0300

    <ADD> createUseri and findUserByEmail methods

--- src/main/java/com/sirun/api_rest/user/infractucture/repositories/RepositoryUser.java
+++ src/main/java/com/sirun/api_rest/user/infractucture/repositories/RepositoryUser.java
@@ -1,28 +1,59 @@
 package com.sirun.api_rest.user.infractucture.repositories;
 
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.sirun.api_rest.user.application.IUseCaseUser;
+import com.sirun.api_rest.user.infractucture.repositories.entities.EntityUser;
+import com.sirun.api_rest.user.infractucture.repositories.parseClass.EntityUserToUser;
+import com.sirun.api_rest.user.infractucture.repositories.parseClass.UserToEntityUser;
 import com.sirun.api_rest.user.model.User;
 
-/**
- * RepositoryUser
- */
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+
+@Repository
 public class RepositoryUser implements IUseCaseUser{
 
+  @PersistenceContext
+  private EntityManager entityManager;
+
   @Override
-  public User create(User user) {
+  @Transactional
+  public User createUser(User user) {
+    EntityUser entityUser = UserToEntityUser.toEntityUser(user); 
+    entityManager.merge(entityUser);  
     return user;
   }
 
+
   @Override
-  public User update(User user) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'update'");
+  @Transactional
+  public User findUserByEmail(String email) {
+    String param = "FROM EntityUser WHERE email = :email";
+    List<EntityUser> entityUsers = entityManager.
+                                    createQuery(param).
+                                    setParameter("email",email).
+                                    getResultList();
+
+   if(entityUsers.size() == 0) return new User();
+    
+   return EntityUserToUser.toUser(entityUsers.get(0));
   }
 
   @Override
-  public void delete(String email) {
+  public User updateUser(User user) {
     // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    throw new UnsupportedOperationException("Unimplemented method 'updateUser'");
+  }
+
+  @Override
+  public void deleteUser(String email) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'deleteUser'");
   }
 
   
