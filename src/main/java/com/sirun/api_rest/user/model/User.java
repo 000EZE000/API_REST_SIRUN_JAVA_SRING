3367cd1679c2cb702ad9f62ae364237bc2cac005
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-23 00:48:18 -0300

    <REFACTOR> set value property id to UUID add constructor

--- src/main/java/com/sirun/api_rest/user/model/User.java
+++ src/main/java/com/sirun/api_rest/user/model/User.java
@@ -1,23 +1,27 @@
 package com.sirun.api_rest.user.model;
 
+import java.util.UUID;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @ToString
 public class User implements IUser {
+  @Getter 
+  private String id = UUID.randomUUID().toString();
   @Getter @Setter
   private String name;
   @Getter @Setter
   private String lastName;
-  @Getter @Setter
+  @Getter 
   private String email;
   @Getter @Setter
   private String password;
   @Getter @Setter
   private String photo = GetPhotoDefault.get();
   @Getter
-  private Roles  rol = Roles.CLIENT;
+  private Roles rol = Roles.CLIENT;
 
   public User(String name, String lastName, String email, String password) {
     this.name = name;
@@ -25,7 +29,16 @@ public class User implements IUser {
     this.email = email;
     this.password = password;
   }
-public User(){}
+  
+  public User(){}
 
-
+  public User(String id,String email,String photo,Roles rol ,String name, String lastName, String password) {
+    this.id = id;
+    this.name = name;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.rol = rol;
+    this.photo = photo;
+  }  
 }
