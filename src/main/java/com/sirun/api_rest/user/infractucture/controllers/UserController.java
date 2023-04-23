4df897a87dcfbd21ebc1bb667d3bdca7b609c667
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2023-04-22 23:43:57 -0300

    <REFACTOR> SignUp method error handling and return correct statuses

--- src/main/java/com/sirun/api_rest/user/infractucture/controllers/UserController.java
+++ src/main/java/com/sirun/api_rest/user/infractucture/controllers/UserController.java
@@ -1,11 +1,15 @@
 package com.sirun.api_rest.user.infractucture.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sirun.api_rest.user.infractucture.services.ServicesUser;
 import com.sirun.api_rest.user.model.User;
 
 @RestController
@@ -13,9 +17,16 @@ import com.sirun.api_rest.user.model.User;
 @CrossOrigin("*")
 public class UserController {
 
-  @PostMapping("sign_up")
-  public User SignUP(@RequestBody User request){
-    return request;
-  } 
+  @Autowired
+  ServicesUser servicesUser;
   
+  @PostMapping("sign_up")
+  public ResponseEntity<String> SignUP(@RequestBody User request){
+    try {
+      servicesUser.create(request);
+    } catch(IllegalArgumentException error){
+      return new ResponseEntity<String>(error.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    return new ResponseEntity<String>("the user has successfully registered", HttpStatus.CREATED);
+  } 
 }
